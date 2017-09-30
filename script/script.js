0781eb49de309c993cde1dcccfa8f5ddefbbe365
Naveen Alphonse <jbapraveen@hotmail.com>
2017-09-30 13:28:15 +0530

    Added Features

--- script/script.js
+++ script/script.js
@@ -1,22 +1,31 @@
 $(document).ready(function() {
 
-    var accessToken = "EAACEdEose0cBAPK3PSRXg5haTp8ip2TpqUrUrg7bJZAfHenV6tEI0V0gAZBwgUX9K1Gegpwj71pdQVZBrOEVOKn8ox0zf987Lk5IhNzLplPAs5vJ3h6HxsZBX6QYsbQ39NQw0P6kV6jfChjhaZB1yBfaBug8AZCwLZBg8jhkKAzjHvXZCpzYXYZA4rtZBwXxcJQjOfd1nNyBGE8QZDZD";
-    $.ajax('https://graph.facebook.com/me?access_token='+accessToken+'&fields=id,name,email,age_range,gender,birthday,work', {
+    var accessToken = "";
+    $.ajax('https://graph.facebook.com/me?access_token='+accessToken+'&fields=id,name,email,age_range,education,hometown,gender,birthday,work,about,relationship_status,religion,political', {
 
         success: printUserData
     });
 
 });
 
-
+console.log(response.hometown.town);
 function printUserData(response){
     console.log(response);
     $("#name").text(response.name);
-    $("#work").text(response.work[0].position.name + " at " + response.work[0].employer.name );//Add for each later
+    $.each(response.work, function(index, value){
+        $("#work").text(value.position.name + " at " + value.employer.name );
+    });
+    $.each(response.education, function(index, value){
+        $(".education-container").append("<h3>"+value.type+"</h3>");
+        $(".education-container").append("<h4>"+value.school.name+"</h3>");
+        
+        //$(".education-container").append("<h4>Class of "+value.year.name+"</h3>");
+    });
+    $("#relationship-status").text(response.relationship_status);
+    $("#about").text(response.about);
+    $("#hometown").text(response.hometown.name);
     $("#age").text(response.age_range.min);
     $("#gender").text(response.gender);
     $("#email").text(response.email);   
-    $("#birthday").text(response.birthday); 
-        
-
+    $("#birthday").text(response.birthday);       
 }
